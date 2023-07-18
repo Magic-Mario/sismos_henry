a4b70772bd78a2ef21a5e10e0e98ea0ff080d0f9
picassojp <picassojuanpablo@gmail.com>
2023-07-18 19:37:18 -0300

    prueba_sismobot cambio frecuencia

--- telegram_bot/prueba_sismobot_grupal.py
+++ telegram_bot/prueba_sismobot_grupal.py
@@ -3,59 +3,83 @@ from pymongo.server_api import ServerApi
 from bson import ObjectId
 import datetime
 import os
+import schedule
+import time
+import random
 
 
-# definición de constantes
-#clave para acceder a mongo
-# password = os.getenv("MONGODB_PASSWORD")
-#print(password)
-password = 'picassojp'
+# Crear un diccionario para mapear los IDs a las opciones de 'place'
+places_por_id = {
+    "64b6b4eca42c41e512af5b28": [
+        "PRUEBABOT: 27 km E of Cordova, Alaska",
+        "PRUEBABOT: 9 km WNW of Cobb, CA",
+        "PRUEBABOT: 4 km SE of Pāhala, Hawaii"
+    ],
+    "64b6aa60a42c41e512af5b27": [
+        "PRUEBABOT: Volcano Islands, Japan region",
+        "PRUEBABOT: 11 km SSE of Asahi, Japan",
+        "PRUEBABOT: 27 km SE of Shinmachi, Japan"
+    ],
+    "64b6a46fa42c41e512af5b26": [
+        "PRUEBABOT: 47 km W of Valparaíso, Chile",
+        "PRUEBABOT: 33 km WNW of Hacienda La Calera, Chile",
+        "PRUEBABOT: 31 km ENE of Chicureo Abajo, Chile"
+    ]
+}
 
-# Crear un nuevo cliente y conectarse al servidor
-uri = f"mongodb+srv://picassojp:{password}@cluster0.cchanol.mongodb.net/?retryWrites=true&w=majority"
-client = MongoClient(uri, server_api=ServerApi('1'))
+# Crear una función para el trabajo que quieres realizar
+def actualizar_mongo():
 
-db = client["pf-henry"]
-collection = db["db-pf-henry"]
+    # definición de constantes
+    #clave para acceder a mongo
+    # password = os.getenv("MONGODB_PASSWORD")
+    #print(password)
+    password = 'picassojp'
 
-# Definir los IDs de los documentos a actualizar y los nuevos valores de 'time'
-ids_a_actualizar = [
-    "64b6a46fa42c41e512af5b26",
-    "64b6aa60a42c41e512af5b27",
-    "64b6b4eca42c41e512af5b28",
-    "64b6b551a42c41e512af5b29",
-    "64b6b595a42c41e512af5b2a",
-    "64b6b5b3a42c41e512af5b2b",
-    "64b6b5cba42c41e512af5b2c",
-    "64b6b5e0a42c41e512af5b2d",
-    "64b6b5f1a42c41e512af5b2e"
-]
+    # Crear un nuevo cliente y conectarse al servidor
+    uri = f"mongodb+srv://picassojp:{password}@cluster0.cchanol.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    db = client["pf-henry"]
+    collection = db["db-pf-henry"]
+
+    # Definir los IDs de los documentos a actualizar y los nuevos valores de 'time'
+    ids_a_actualizar = [
+        "64b6a46fa42c41e512af5b26",
+        "64b6aa60a42c41e512af5b27",
+        "64b6b4eca42c41e512af5b28"
+    ]
 
 
-# Definir los nuevos valores para 'time'
-nuevos_valores_time = [
-    datetime.datetime(2023, 7, 18, 21, 30, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 31, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 32, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 33, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 34, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 35, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 36, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 37, 2, 632905),
-    datetime.datetime(2023, 7, 18, 21, 38, 2, 632905)
-]
+    # Crear la lista de nuevos tiempos con intervalos de 30 segundos a partir del tiempo actual
+    #tiempo_actual = datetime.datetime.now()
+    tiempo_actual = datetime.datetime.utcnow()  # Usar utcnow en lugar de now
 
-# Crear la lista de nuevos tiempos con intervalos de 30 segundos a partir del tiempo actual
-#tiempo_actual = datetime.datetime.now()
-tiempo_actual = datetime.datetime.utcnow()  # Usar utcnow en lugar de now
+    print(tiempo_actual)
+    nuevos_valores_time = [
+        tiempo_actual + datetime.timedelta(seconds=i*15)
+        for i in range(len(ids_a_actualizar))] #lista con los valores de tiempo a reemplazar con el formato requerido por BSON
 
-print(tiempo_actual)
-# nuevos_valores_time = [
-#     tiempo_actual + datetime.timedelta(seconds=i*30)
-#     for i in range(len(ids_a_actualizar))] #lista con los valores de tiempo a reemplazar con el formato requerido por BSON
+    print(nuevos_valores_time)
 
-print(nuevos_valores_time)
+    # Iterar sobre los IDs y actualizar cada documento
+    for id_, nuevo_time in zip(ids_a_actualizar, nuevos_valores_time):
+        # Generar valores aleatorios para 'dep' y 'mag'
+        nuevo_dep = random.randint(10, 999)
+        nuevo_mag = random.uniform(1, 10)  # Esto genera un float entre 1 y 10
 
-# Iterar sobre los IDs y actualizar cada documento
-for id_, nuevo_time in zip(ids_a_actualizar, nuevos_valores_time):
-    collection.update_one({"_id": ObjectId(id_)}, {"$set": {"time": nuevo_time}})
+        # Seleccionar una opción aleatoria de 'place' para este ID
+        nuevo_place = random.choice(places_por_id[id_])
+
+        # Actualizar el documento
+        collection.update_one(
+            {"_id": ObjectId(id_)},
+            {"$set": {"time": nuevo_time, "depth": nuevo_dep, "mag": nuevo_mag, "place": nuevo_place}}
+        )
+# Configurar schedule para ejecutar tu función cada 2 minutos
+schedule.every(1).minutes.do(actualizar_mongo)
+
+# Ejecutar schedule en un bucle infinito
+while True:
+    schedule.run_pending()
+    time.sleep(1)        
